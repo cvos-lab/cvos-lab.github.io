613215bf2169b8190ba040a1e7f942cbd5f16218
Hieu Nguyen <66098583+cvos-lab@users.noreply.github.com>
2023-10-25 14:08:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+let slider1 = tns({
+  container: ".my-slider1",
+  "slideBy": "1",
+  "speed": 400,
+  "nav": false,
+  autoplay: true,
+  controls: false,
+  autoplayButtonOutput: false,
+  responsive: {
+    1600: {
+      items: 4,
+      gutter: 20
+    },
+    1024: {
+      items: 3,
+      gutter: 20
+    },
+    768: {
+      items: 2,
+      gutter: 20
+    },
+    480: {
+      items: 1,
+      gutter: 20
+    }
+  }
+})
+
+let slider2 = tns({
+  container: ".my-slider2",
+  "slideBy": "2",
+  "speed": 400,
+  "nav": false,
+  autoplay: true,
+  controls: false,
+  autoplayButtonOutput: false,
+  responsive: {
+    1600: {
+      items: 4,
+      gutter: 20
+    },
+    1024: {
+      items: 3,
+      gutter: 20
+    },
+    768: {
+      items: 2,
+      gutter: 20
+    },
+    480: {
+      items: 1,
+      gutter: 20
+    }
+  }
+})
+
+let slider3 = tns({
+  container: ".my-slider3",
+  "slideBy": "2",
+  "speed": 400,
+  "nav": false,
+  autoplay: true,
+  controls: false,
+  autoplayButtonOutput: false,
+  responsive: {
+    1600: {
+      items: 4,
+      gutter: 20
+    },
+    1024: {
+      items: 3,
+      gutter: 20
+    },
+    768: {
+      items: 2,
+      gutter: 20
+    },
+    480: {
+      items: 1,
+      gutter: 20
+    }
+  }
+})
